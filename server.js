94f296e38ddfd31e87c02e89a0c27a9d4efafdc0
Aurora MS <aurorams94@gmail.com>
2020-08-11 12:58:43 +0200

    Creación de la estructura basica en server y creación de el header y el nav principal

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express')
+pug = require('pug');
+const app = express()
+
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index.pug'); 
+  });
+
+app.listen(process.env.PORT)
